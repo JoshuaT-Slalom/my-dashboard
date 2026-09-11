367408803ff25d84f43382acc60d7a0fadcc0fd6
Josh Thompson <joshuat@slalom.com>
2026-09-11 09:03:22 -0500

    Adding V3 components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import router from './router'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+const vuetify = createVuetify({
+	icons: {
+		defaultSet: 'mdi',
+		aliases,
+		sets: { mdi },
+	},
+})
+
+createApp(App).use(vuetify).use(router).mount('#app')
